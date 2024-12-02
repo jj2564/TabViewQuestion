9da6f43c917dab78b593ccc39c97d8646c756ade
irving.huang <irving.hy.huang@vivotek.com>
2024-12-02 10:22:54 +0800

    Initial Commit

---
+++ TabViewQuestion/TabViewQuestionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewQuestionApp.swift
+//  TabViewQuestion
+//
+//  Created by IrvingHuang on 2024/12/2.
+//
+
+import SwiftUI
+
+@main
+struct TabViewQuestionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
